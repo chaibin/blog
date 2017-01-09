da873a892037baef8621b17e08efc52afd202013
chaidongbin <chaidongbin@yuanxin-inc.com>
2017-01-09 13:40:32 +0800

    local

---
+++ conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+admin:$1$Aukxqytn$CZ3Me46.0u.5LZDEuoRUp.:admin:471793789@qq.com:admin,user
